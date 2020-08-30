4f894ab47b2cff90e995b2899ea472cd1f83faac
together08 <2958537424@qq.com>
2020-08-30 10:49:50 +0800

    setup.sh: Track modified version of ErfanGSIs

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone --recurse-submodules https://github.com/erfanoabdi/ErfanGSIs.git --depth=1 erfangsi
+git clone --recurse-submodules https://github.com/together08/ErfanGSIs-Mod.git --depth=1 erfangsi
 bash ./erfangsi/setup.sh
